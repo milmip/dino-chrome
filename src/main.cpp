10f7f0695880c277810c7e9f8e660f4f0ebfa398
milmip <emilien.progin@gmail.com>
2024-04-03 15:34:03 +0200

    connect sfml to the project

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,38 @@
 Source code of DINOCHROME by MilMip
 start 3.4.24
 
-compil command :
-g++ src/main.cpp -o bin/prog && bin/prog
+lib doc:
+https://www.sfml-dev.org/tutorials/2.6/start-linux.php
+https://www.sfml-dev.org/tutorials/2.6/
+
+compil command:
+cd bin && g++ -c ../src/main.cpp && g++ main.o -o prog -lsfml-graphics -lsfml-window -lsfml-system  && cd .. && bin/prog
 */
 
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout << "Hello world!" << std::endl;
-	return 0;
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    std::cout << "loaded\n";
+
+    return 0;
 }
