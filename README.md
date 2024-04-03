0299c37ff3a3fa91c2261b7165c9750d65f32098
milmip <emilien.progin@gmail.com>
2024-04-03 15:06:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dino from Chrome
+
+First litle project using c++.
+
+We gonna recreate the dino of google Chorme from scratch.
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+Source code of DINOCHROME by MilMip
+start 3.4.24
+
+compil command :
+g++ src/main.cpp -o bin/prog && bin/prog
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello world!" << std::endl;
+	return 0;
+}
